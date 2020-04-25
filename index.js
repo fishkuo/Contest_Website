b44724739818bd06343d06cb0551ca167b67945c
Aaron Chen <aaron880221@gmail.com>
2020-04-26 01:39:46 +0800

    Update rating form

--- index.js
+++ index.js
@@ -10,61 +10,58 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 function getConnection() {
-	var content = fs.readFileSync('user.json').toString('utf8');
-	var { user, password } = JSON.parse(content);
+    var content = fs.readFileSync('user.json').toString('utf8');
+    var { user, password } = JSON.parse(content);
 
-	return mysql.createConnection({
-		host: 'db.cbgo4t4xbjoi.ap-southeast-1.rds.amazonaws.com',
-		user: user,
-		password: password,
-		database: 'db'
-	});
+    return mysql.createConnection({
+        host: 'db.cbgo4t4xbjoi.ap-southeast-1.rds.amazonaws.com',
+        user: user,
+        password: password,
+        database: 'db'
+    });
 }
 
 app.post('/add_review', (req, res) => {
-	var id = parseInt(req.body.id);
-	var rating = parseInt(req.body.rating);
-	const QUERY_STRING =
-		'UPDATE review SET rating_amount = rating_amount + 1, rating_score = rating_score + ?,\
-                                            rating_average = rating_score / rating_amount WHERE review_id = ?';
+    let date_ob = new Date();
+    var id = req.body.name;;
+    var rating = parseInt(req.body.rating);
 
-	getConnection().query(QUERY_STRING, [ rating, id ], (err, result, field) => {
-		if (err) {
-			console.log('Query Failed: ', err);
-			res.sendStatus(500);
-			return;
-		}
-		// console.log("Query Successed!");
-		res.end();
-	});
-	res.redirect('/table.html');
-	res.end();
+    const QUERY_STRING1 = "INSERT INTO `childMessage` (name, date, message, rating) VALUES (?, ?, ?, ?)";
+    var date = date_ob.getFullYear() + "-" + ("0" + (date_ob.getMonth() + 1)).slice(-2) + "-" + ("0" + date_ob.getDate()).slice(-2);
+    getConnection().query(QUERY_STRING1, [id, date, '', rating], (err, result, field) => {
+        if (err) {
+            console.log('Query Failed: ', err);
+            res.sendStatus(500);
+            return;
+        }
+        res.end();
+    });
+
+    const QUERY_STRING2 = "UPDATE childCareSystem SET rating_amount = rating_amount + 1, \
+                          rating_score = rating_score + ?, rating_average = rating_score / rating_amount WHERE name = ?";
+    getConnection().query(QUERY_STRING2, [rating, id], (err, result, field) => {
+        if (err) {
+            console.log('Query Failed: ', err);
+            res.sendStatus(500);
+            return;
+        }
+        res.end();
+    });
+    res.redirect('/table.html');
+    res.end();
 });
 
 app.get('/get_reviews', (req, res) => {
-	const QUERY_STRING =
-		'SELECT review_id, name, rating_amount, rating_score, rating_average \
-                            FROM review ';
-	getConnection().query(QUERY_STRING, (err, result, field) => {
-		if (err) {
-			console.log('Query Failed: ', err);
-			res.sendStatus(500);
-			return;
-		}
-		res.send(result);
-	});
-});
-
-app.get('/get_child', (req, res) => {
-	const QUERY_STRING = 'SELECT name FROM review ';
-	getConnection().query(QUERY_STRING, (err, result, field) => {
-		if (err) {
-			console.log('Query Failed: ', err);
-			res.sendStatus(500);
-			return;
-		}
-		res.send(result);
-	});
+    const QUERY_STRING =
+        'SELECT id, name, rating_amount, rating_score, rating_average FROM childCareSystem';
+    getConnection().query(QUERY_STRING, (err, result, field) => {
+        if (err) {
+            console.log('Query Failed: ', err);
+            res.sendStatus(500);
+            return;
+        }
+        res.send(result);
+    });
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
