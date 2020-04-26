e3f6bdc16c9a723c29104e8bfdf521ece50be13f
Aaron Chen <aaron880221@gmail.com>
2020-04-26 13:53:25 +0800

    Complete select onchange form

--- index.js
+++ index.js
@@ -1,67 +1,84 @@
-const fs = require('fs');
-const path = require('path');
-const mysql = require('mysql');
-const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require("fs");
+const path = require("path");
+const mysql = require("mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 function getConnection() {
-    var content = fs.readFileSync('user.json').toString('utf8');
+    var content = fs.readFileSync("user.json").toString("utf8");
     var { user, password } = JSON.parse(content);
 
     return mysql.createConnection({
-        host: 'db.cbgo4t4xbjoi.ap-southeast-1.rds.amazonaws.com',
+        host: "db.cbgo4t4xbjoi.ap-southeast-1.rds.amazonaws.com",
         user: user,
         password: password,
-        database: 'db'
+        database: "db",
     });
 }
 
-app.post('/add_review', (req, res) => {
+app.post("/add_review", (req, res) => {
     let date_ob = new Date();
-    var id = req.body.name;;
+    var id = req.body.name;
     var rating = parseInt(req.body.rating);
 
-    const QUERY_STRING1 = "INSERT INTO `childMessage` (name, date, message, rating) VALUES (?, ?, ?, ?)";
-    var date = date_ob.getFullYear() + "-" + ("0" + (date_ob.getMonth() + 1)).slice(-2) + "-" + ("0" + date_ob.getDate()).slice(-2);
-    getConnection().query(QUERY_STRING1, [id, date, '', rating], (err, result, field) => {
-        if (err) {
-            console.log('Query Failed: ', err);
-            res.sendStatus(500);
-            return;
+    const QUERY_STRING1 =
+        "INSERT INTO `childMessage` (name, date, message, rating) VALUES (?, ?, ?, ?)";
+    var date =
+        date_ob.getFullYear() +
+        "-" +
+        ("0" + (date_ob.getMonth() + 1)).slice(-2) +
+        "-" +
+        ("0" + date_ob.getDate()).slice(-2);
+    getConnection().query(
+        QUERY_STRING1,
+        [id, date, "", rating],
+        (err, result, field) => {
+            if (err) {
+                console.log("Query Failed: ", err);
+                res.sendStatus(500);
+                return;
+            }
+            res.end();
         }
-        res.end();
-    });
+    );
 
-    const QUERY_STRING2 = "UPDATE childCareSystem SET rating_amount = rating_amount + 1, \
+    const QUERY_STRING2 =
+        "UPDATE childCareSystem SET rating_amount = rating_amount + 1, \
                           rating_score = rating_score + ?, rating_average = rating_score / rating_amount WHERE name = ?";
     getConnection().query(QUERY_STRING2, [rating, id], (err, result, field) => {
         if (err) {
-            console.log('Query Failed: ', err);
+            console.log("Query Failed: ", err);
             res.sendStatus(500);
             return;
         }
         res.end();
     });
-    res.redirect('/table.html');
+    res.redirect("/table.html");
     res.end();
 });
 
-app.get('/get_reviews', (req, res) => {
+app.get("/get_reviews", (req, res) => {
     const QUERY_STRING =
-        'SELECT id, name, rating_amount, rating_score, rating_average FROM childCareSystem';
+        "SELECT id, name, rating_amount, rating_score, rating_average FROM childCareSystem";
     getConnection().query(QUERY_STRING, (err, result, field) => {
         if (err) {
-            console.log('Query Failed: ', err);
+            console.log("Query Failed: ", err);
             res.sendStatus(500);
             return;
         }
         res.send(result);
     });
+});
+
+app.get("/test", (req, res) => {
+    var content = fs.readFileSync("test.json").toString("utf8");
+    var cityDict = JSON.parse(content);
+    res.send(cityDict[req.query.city]);
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
